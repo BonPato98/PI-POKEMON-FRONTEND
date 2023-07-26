7a2afac02534633c9b1e3d9e40ba4bb45659e956
BonPato98 <bonfiglipatricio12@hotmail.com>
2023-07-26 15:07:06 -0300

    deploy

--- client/src/index.js
+++ client/src/index.js
@@ -4,10 +4,6 @@ import './index.css';
 import App from './App';
 import {Provider} from 'react-redux';
 import {store} from './Redux/Store'
-import axios from 'axios'
-
-// axios.defaults.baseURL = 'http://localhost:3001'
-axios.defaults.baseURL = 'pi-pokemon-backend-production.up.railway.app'
 
 ReactDOM.render(
   // <React.StrictMode>
